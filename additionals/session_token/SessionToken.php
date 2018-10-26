bf56cbcd4c6f05d910e6e16f3679840d54452656
leplutonien <plutoniendu1erdec@gmail.com>
2018-10-26 18:16:54 +0100

    Change Session logic

--- additionals/session_token/SessionToken.php
+++ additionals/session_token/SessionToken.php
@@ -8,60 +8,142 @@ use lib\sql\SqlQuery;
  * Manage session token by using Json Web Token
  *@author PhpGenerator (https://github.com/leplutonien/php_generator)
  */
-
-
 class SessionToken{
-    private static $token_storage = "/additionals/sessions.json";
-    private  static  $secret_server_key = "neinotulpeBJl1960";
-    private $token ;
-    private static  $validity = 60 * 2 ; //15min
+    private static $sessionToken;
+    private $secret_server_key = "neinotulpeBJl1960";
+    private $token_validity = 60 * 30 ; //30min
+    private $access_code_validity = 60 * 1440 ; //24h
+    private $pwd_reset_token_validity = 60 * 1440 ; //24h
+    private $current_token = null;
+    private $current_id_session = null;
 
+    public function __construct($token = null){
+        $this->current_token = $token;
+        self::$sessionToken = $this;
+    }
 
-    public static  function create($token){
-        $token = JWT::encode($token,SessionToken::$secret_server_key);
-        SessionToken::clean();
+    public function setIdSession($id){
+        $this->current_id_session = $id;
+    }
+
+    public static function getSessionToken(){
+        return self::$sessionToken;
+    }
+
+    /**
+     * create new instance of session
+     */
+    public function create($user){
+        $this->clean();
 
         $csession = Sessions::findByAttribute(array(
-            "token"=> $token
+            "id_user"=> $user
         ));
 
         if( empty($csession) || count($csession) == 0 ){
             $nsession = new Sessions(
                 array(
                     "id" => (int)BoxFunctions::AutomaticNumber('sessions','id',null,5),
-                    "token"=> $token,
+                    "id_user"=> $user,
                     "timestamp"=> time()
                 )
             );
             $nsession->insert();
-            return $token;
+            $this->current_id_session =  $nsession->getId();
+            return $nsession->getId();
         }else{
-            return $csession[0]->getToken();
+            $this->current_id_session = $csession[0]->getId();
+            return $csession[0]->getId();
         }
     }
 
-    public static function sessionInfo($token){
-        SessionToken::clean();
+    public function setToken($data){
+        $token = JWT::encode($data,$this->secret_server_key);
+        $this->clean();
 
         $csession = Sessions::findByAttribute(array(
-            "token"=> $token
+            "id"=> $this->current_id_session
+        ));
+
+        if(!is_null($csession) && count($csession) == 1){
+            $s =  $csession[0];
+            $s->setToken($token);
+            $s->setTimestamp(time());
+            $s->update();
+            $this->current_token = $token;
+            return $token;
+        }else{
+            return null;
+        }
+    }
+
+    /**
+     * set access control code
+     */
+    public function setAccessControlCode($code){
+        $this->clean();
+        $csession = Sessions::findByAttribute(array(
+            "id"=> $this->current_id_session
         ));
 
         if( empty($csession) || count($csession) == 0 )
-            return null;
+            return false;
         else {
-           $s =  $csession[0];
+            $s =  $csession[0];
+            $s->setAccessControlCode($code);
             $s->setTimestamp(time());
-            $s->update();
-            return JWT::decode($token, SessionToken::$secret_server_key);
+            return $s->update();
         }
     }
 
-    public static function remove($token){
-        SessionToken::clean();
+    /**
+     * set pwd reset token
+     */
+    public function setPwdResetToken($reset_token){
+        $this->clean();
+        $csession = Sessions::findByAttribute(array(
+            "id"=> $this->current_id_session
+        ));
+
+        if( empty($csession) || count($csession) == 0 )
+            return false;
+        else {
+            $s =  $csession[0];
+            $s->setPwdResetToken($reset_token);
+            $s->setTimestamp(time());
+            return $s->update();
+        }
+    }
+
+    public function sessionInfo(){
+        $this->clean();
+        return JWT::decode($this->current_token,
+            $this->secret_server_key);
+    }
+
+    public function updateTime(){
+        $this->clean();
+        $csession = Sessions::findByAttribute(array(
+            "token"=> $this->current_token
+        ));
+
+        if( empty($csession) || count($csession) == 0 )
+            return false;
+        else {
+            $s =  $csession[0];
+            $s->setTimestamp(time());
+            return $s->update();
+        }
+    }
+
+    /**
+     * remove session via specific token
+     */
+    public function remove(){
+        $this->clean();
 
         $csession = Sessions::findByAttribute(array(
-            "token"=> $token
+            "token"=> $this->current_token
         ));
 
         if( !empty($csession) && count($csession) == 1 )
@@ -71,16 +153,28 @@ class SessionToken{
     }
 
     /**
-     *
-    Deletes all sessions whose time variable is less than
+     *Deletes all sessions whose time variable is less than
+     * and update token
      * @return bool
      * @throws \Exception
      */
-    public static function clean(){
+    public function clean(){
+        //delete instances
         $sql = " DELETE FROM sessions WHERE timestamp < :time";
         $sqlQuery = new SqlQuery($sql);
-        $sqlQuery->setParams('time',time() - SessionToken::$validity);
+        $sqlQuery->setParams('time',time() - $this->token_validity);
         $resultSet = $sqlQuery->execute();
-        return $resultSet->getStatus();
+
+        //update access_control_code
+        $sql = " UPDATE sessions set access_control_code = null WHERE timestamp < :time";
+        $sqlQuery = new SqlQuery($sql);
+        $sqlQuery->setParams('time',time() - $this->access_code_validity);
+        $resultSet = $sqlQuery->execute();
+
+        //update pwd_reset_token
+        $sql = " UPDATE sessions set pwd_reset_token = null WHERE timestamp < :time";
+        $sqlQuery = new SqlQuery($sql);
+        $sqlQuery->setParams('time',time() - $this->pwd_reset_token_validity);
+        $resultSet = $sqlQuery->execute();
     }
 }
--- additionals/session_token/sessions.sql
+++ additionals/session_token/sessions.sql
@@ -1,7 +1,10 @@
 DROP TABLE IF EXISTS sessions ;
-CREATE TABLE sessions(
-	id INT(3) NOT NULL,
-	timestamp int(11) NOT NULL,
-	token VARCHAR(250) NOT NULL,
-	PRIMARY KEY (id)
-)ENGINE=InnoDB;
+  CREATE TABLE sessions(
+    id INT(3) NOT NULL,
+    id_user INT(3) NOT NULL UNIQUE,
+    timestamp int(11) NOT NULL,
+    token VARCHAR(250)  UNIQUE,
+    access_control_code VARCHAR(250) UNIQUE,
+    pwd_reset_token VARCHAR(250) UNIQUE,
+    PRIMARY KEY (id)
+  );
